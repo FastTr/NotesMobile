d76335652679ae8828b3af62f4645836627d41af
J Hwang <hwangjk012@hotmail.com>
2019-12-19 19:51:33 -0600

    Initialize Spring Boot Kotlin Project
    - Add Controller Folder
    - Add Repository Folder
    - Add Service Folder

---
+++ KotlinSpring/notesmobile/src/main/kotlin/com/fastr/notesmobile/NotesmobileApplication.kt
@@ -0,0 +1,11 @@
+package com.fastr.notesmobile
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NotesmobileApplication
+
+fun main(args: Array<String>) {
+	runApplication<NotesmobileApplication>(*args)
+}
---
+++ KotlinSpring/notesmobile/src/test/kotlin/com/fastr/notesmobile/NotesmobileApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fastr.notesmobile
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NotesmobileApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
