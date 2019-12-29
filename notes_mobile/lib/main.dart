bdb4a73bfbea9d39afb05596ee2038dcbc407ee1
AnubhawM <anubo16@gmail.com>
2019-12-29 11:10:18 -0600

    Basic functionality of app done, need to add on-device saving

--- notes_mobile/lib/main.dart
+++ notes_mobile/lib/main.dart
@@ -1,41 +1,48 @@
+// Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Notes',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Notes'),
     );
   }
 }
 
+buttonPressed() {
+  print("BUTTON CLICKED");
+}
+
+Map<String, String> notesMap = new Map<String, String>();
+
+// TODO: Add new notes to a ListView dynamically
+Widget myListView(BuildContext context, Map<String, String> notesMap) {
+
+  return new ListView.builder(
+    itemCount: notesMap.length,
+    itemBuilder: (BuildContext context, int index) {
+      return ListTile(
+        title: Text("${notesMap.keys.elementAt(index)}"),
+        subtitle: Text("${notesMap[notesMap.keys.elementAt(index)]}"),
+      );
+    },
+  );
+
+}
+
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -43,69 +50,159 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    
+    ListView drawerListView = new ListView(
+      children: <Widget>[
+        new OutlineButton(
+            padding: new EdgeInsets.all(15.0),
+            child: new Text("Spreadsheet",
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                )),
+            onPressed: () {
+              null;
+            },
+            textColor: Colors.green),
+
+        new OutlineButton(
+            padding: new EdgeInsets.all(15.0),
+            child: new Text("Calendar",
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                )),
+            onPressed: () {
+              null;
+            },
+            textColor: Colors.green),
+
+        new OutlineButton(
+            padding: new EdgeInsets.all(15.0),
+            child: new Text("Calculator",
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                )),
+            onPressed: () {
+              null;
+            },
+            textColor: Colors.green)            
+
+      ],
+    );
+
+    Drawer drawer = new Drawer(child: drawerListView);
+
+    return Scaffold(
+        drawer: drawer,
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: myListView(context, notesMap),
+
+        floatingActionButton: new FloatingActionButton(
+          onPressed: (){
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => NewNotePage()),
+            );
+          },
+          tooltip: "Make a new note",
+          child: new Icon(Icons.add),
+        )
+
+        );
+  }
+}
+
+
+class NewNotePage extends StatelessWidget{
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    final TextEditingController notesTitleController = new TextEditingController();
+    final TextEditingController notesContentController = new TextEditingController();
+    String noteTitle = "";
+    String noteContent = "";
+
+
+
+
+  // void disposeNotesTitle() {
+  //   // Clean up the controller when the widget is disposed.
+  //   notesTitleController.dispose();
+  //   super.dispose();
+  // }
+
+  // void disposeNotesContent() {
+  //   notesContentController.dispose();
+  //   super.dispose();
+  // }
+    
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("New Note"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+      body: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: new Column(
+            children: <Widget>[
+              TextField(
+                  controller: notesTitleController,
+                  maxLines: 1,
+                  decoration: InputDecoration(
+                    hintText: "Enter note title here",
+                    border: OutlineInputBorder(),
+                  )),
+              Container(height: 20),
+              TextField(
+                controller: notesContentController,
+                maxLines: 2,
+                decoration: InputDecoration(
+                  hintText: "Enter note here",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              Container(height: 15),
+              new OutlineButton(
+                  padding: new EdgeInsets.all(15.0),
+                  child: new Text("Finish note",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      )),
+                      // Figure out how to make the new note show up after clicking submit button
+                  onPressed: () {
+                    
+                    Navigator.pop(context);
+                    notesMap[notesTitleController.text] = notesContentController.text;
+                    // setState() {
+
+                    // }                    
+                  },
+                  
+                  textColor: Colors.green),
+
+                  // new Expanded(child: new ListView.builder(
+                  //   itemCount: notesMap.length, 
+                  //   itemBuilder: (BuildContext context, int index) {
+                  //     _myListView(context, notesTitleController.text, notesContentController.text);
+                  //   },
+                  //   )),
+            ],
+          ),
+          
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+
     );
   }
 }
